c3427d816bfc9f35e47eb9eb1433ff0ff303003f
Jonathan Reichelt Gjertsen <jonathan.gjertsen@disruptive-technologies.com>
2021-07-21 21:35:00 +0200

    Add treeview option and more bans to listfiles program

--- listfiles.c
+++ listfiles.c
@@ -1,10 +1,40 @@
 #include <dirent.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <stdbool.h>
 
 #include "shell_utils.h"
 
-bool iter_dir(const char *dirname, int depth)
+#define OPT_TREEVIEW (1 << 0)
+
+const char BAN_LIST[][sizeof(".pytest_cache")] = {
+    ".pytest_cache",
+    "node_modules",
+    "__pycache__",
+    "CMakeFiles",
+    "_pytest",
+    ".git",
+    ".",
+    ".."
+};
+
+bool is_like_git_cache(const char *name)
+{
+    int name_len = strlen(name);
+    if (name_len != 2)
+    {
+        return false;
+    }
+    char c0 = name[0];
+    char c1 = name[1];
+    bool c0_is_num   = (c0 >= '0' && c0 <= '9');
+    bool c0_is_hexal = (c0 >= 'a' && c0 <= 'f');
+    bool c1_is_num   = (c1 >= '0' && c1 <= '9');
+    bool c1_is_hexal = (c1 >= 'a' && c1 <= 'f');
+    return ((c0_is_num || c0_is_hexal) && (c1_is_num || c1_is_hexal));
+}
+
+bool iter_dir(const char *dirname, int depth, uint32_t options)
 {
     DIR *directory = opendir(dirname);
     if (directory)
@@ -16,15 +46,17 @@ bool iter_dir(const char *dirname, int depth)
             {
                 break;
             }
-            if (strcmp(file->d_name, ".") == 0)
+            bool banned = false;
+            for (int i = 0; i < ARRAY_SIZE(BAN_LIST); i++)
             {
-                continue;
+                if (strcmp(file->d_name, BAN_LIST[i]) == 0)
+                {
+                    banned = true;
+                    break;
+                }
             }
-            if (strcmp(file->d_name, ".git") == 0)
-            {
-                continue;
-            }
-            if (strcmp(file->d_name, "..") == 0)
+            banned |= is_like_git_cache(file->d_name);
+            if (banned)
             {
                 continue;
             }
@@ -32,8 +64,21 @@ bool iter_dir(const char *dirname, int depth)
             strcpy(full_name, dirname);
             strcat(full_name, "/");
             strcat(full_name, file->d_name);
-            printf("%s\n", full_name);
-            iter_dir(full_name, depth + 1);
+            line_buffer shown_name = {};
+            if (options & OPT_TREEVIEW)
+            {
+                for (int i = 0; i < depth * 4; i++)
+                {
+                    shown_name[i] = ' ';
+                }
+                strcat(shown_name, file->d_name);
+            }
+            else
+            {
+                strcpy(shown_name, full_name);
+            }
+            printf("%s\n", shown_name);
+            iter_dir(full_name, depth + 1, options);
         }
         closedir(directory);
         return true;
@@ -46,7 +91,14 @@ bool iter_dir(const char *dirname, int depth)
 
 int main(int argc, char **argv)
 {
-    ensure(argc == 1, "Expect 0 args");
-    iter_dir(".", 0);
+    uint32_t options = 0;
+    for (int i = 1; i < argc; i++)
+    {
+        if (strcmp(argv[i], "--tree") == 0)
+        {
+            options |= OPT_TREEVIEW;
+        }
+    }
+    iter_dir(".", 0, options);
     return 0;
 }
